c1860057090b1f9d08ac407d9cd86a5db7d1c0bf
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-10-16 21:48:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,12 +3,33 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
+from filters import IsAdminFilter
+
 # log level
 logging.basicConfig(level=logging.INFO)
 
 # bot init
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
+
+# activate filters
+dp.filters_factory.bind(IsAdminFilter)
+
+
+@dp.message_handler(is_admin=True, commands=['ban'], commands_prefix='!/')
+async def cmd_ban(message: types.Message):
+    if not message.reply_to_message:
+        await message.reply('This command need to be as reply on message')
+        return
+    # await message.bot.delete_message(config.GROUP_ID, message.message_id)
+    # await message.bot.kick_chat_member(chat_id=config.GROUP_ID, user_id=message.reply_to_message.from_user.id)
+
+    # await message.reply_to_message.reply('User has been banned')
+    await message.reply_to_message.reply('My blade is ready to be unleashed.')
+
+@dp.message_handler(content_types=['new_chat_members'])
+async def on_user_joined(message: types.Message):
+    await message.delete()
 
 
 @dp.message_handler()
