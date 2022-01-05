64fa1ed0cd7e8f93d6e77311b500fafbe73f60ff
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-01-05 19:18:18 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
-from sqlmodel import Field, SQLModel, create_engine, Session
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+from sqlalchemy import exc
 
 
 class User(SQLModel, table=True):
@@ -12,8 +13,13 @@ class User(SQLModel, table=True):
     mutes: Optional[int] = Field(default=0, description='Number of user\'s mutes')
 
 
-def get_user():
-    pass
+def get_user(engine: create_engine, user_id: str) -> Optional[User, None]:
+    with Session(engine) as session:
+        try:
+            user = session.exec(select(User).where(User.user_id == user_id)).one()
+        except exc.NoResultFound:
+            return None
+    return user
 
 
 def create_user(engine: create_engine, data: dict):
@@ -23,12 +29,27 @@ def create_user(engine: create_engine, data: dict):
         session.commit()
 
 
-def update_user():
-    pass
+def update_user(engine: create_engine, user_id: str, data: dict) -> bool:
+    user = get_user(engine, user_id)
+    with Session(engine) as session:
+        try:
+            user = User(**data)
+            session.add(user)
+            session.commit()
+        except exc.CompileError:
+            return False
+    return True
 
 
-def delete_user():
-    pass
+def delete_user(engine: create_engine, user_id: str) -> bool:
+    user = get_user(engine, user_id)
+    with Session(engine) as session:
+        try:
+            session.delete(user)
+            session.commit()
+        except exc.CompileError:
+            return False
+    return True
 
 
 def create_database(engine: create_engine) -> None:
