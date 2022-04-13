5c49a3063cfb7321ad2c9a8351516a3ecf0f65b8
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-04-13 20:54:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import os
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.executor import start_webhook
 
@@ -21,17 +21,22 @@ from tgbot.handlers.ban import register_ban
 from tgbot.handlers.new_chat_members import register_join
 
 logger = logging.getLogger(__name__)
+config = load_config()
+storage = MemoryStorage()
+bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+dp = Dispatcher(bot, storage=storage)
+bot['config'] = config
 
 
 def register_all_middlewares(dp):
     pass
 
 
-def register_all_filters(dp):
+def register_all_filters():
     dp.filters_factory.bind(IsAdminFilter)
 
 
-def register_all_handlers(dp):
+def register_all_handlers():
     register_start(dp)
     register_mute(dp)
     register_ban(dp)
@@ -43,11 +48,11 @@ def register_all_handlers(dp):
     register_join(dp)
 
 
-async def set_all_default_commands(bot: Bot):
+async def set_all_default_commands():
     await set_default_commands(bot)
 
 
-def create_db(bot: Bot):
+def create_db():
     sqlite_file_name = 'database.db'
     sqlite_url = f'sqlite:///{sqlite_file_name}'
     engine = get_engine(sqlite_url)
@@ -55,66 +60,99 @@ def create_db(bot: Bot):
     bot['engine'] = engine
 
 
+# Webhook settings
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{config.tg_bot.token}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# Webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = int(os.getenv('PORT', 5000))
+
+
 # Functions for webhooks
-async def on_startup(bot: Bot, WEBHOOK_URL: str):
+async def on_startup(dp):
+    await set_all_default_commands()
     await bot.set_webhook(WEBHOOK_URL)
 
 
-async def on_shutdown():
+async def on_shutdown(dp):
+    await dp.storage.close()
+    await dp.storage.wait_closed()
     logging.warning('Shutting down..')
     logging.warning('Bye!')
 
 
-async def main():
+# async def main():
+#     logging.basicConfig(
+#         level=logging.INFO,
+#         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
+#     )
+#     logger.info("Starting bot")
+#     config = load_config()
+#
+#     storage = MemoryStorage()
+#     bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+#     dp = Dispatcher(bot, storage=storage)
+#
+#     bot['config'] = config
+#
+#     register_all_middlewares(dp)
+#     register_all_filters(dp)
+#     register_all_handlers(dp)
+#
+#     create_db(bot)
+#
+#     await set_all_default_commands(bot)
+#
+#     # Webhook settings
+#     HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+#     WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+#     WEBHOOK_PATH = f'/webhook/{config.tg_bot.token}'
+#     WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+#
+#     # Webserver settings
+#     WEBAPP_HOST = '0.0.0.0'
+#     WEBAPP_PORT = int(os.getenv('PORT', 5000))
+#
+#     # start
+#     try:
+#         start_webhook(
+#             dispatcher=dp,
+#             webhook_path=WEBHOOK_PATH,
+#             on_shutdown=on_shutdown,
+#             on_startup=on_startup(bot, WEBHOOK_URL),
+#             skip_updates=True,
+#             host=WEBAPP_HOST,
+#             port=WEBAPP_PORT
+#         )
+#     finally:
+#         await dp.storage.close()
+#         await dp.storage.wait_closed()
+
+
+if __name__ == '__main__':
     logging.basicConfig(
         level=logging.INFO,
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
     )
     logger.info("Starting bot")
-    config = load_config()
-
-    storage = MemoryStorage()
-    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
-    dp = Dispatcher(bot, storage=storage)
-
-    bot['config'] = config
 
     register_all_middlewares(dp)
-    register_all_filters(dp)
-    register_all_handlers(dp)
+    register_all_filters()
+    register_all_handlers()
 
-    create_db(bot)
+    create_db()
 
-    await set_all_default_commands(bot)
-
-    # Webhook settings
-    HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-    WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-    WEBHOOK_PATH = f'/webhook/{config.tg_bot.token}'
-    WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-    # Webserver settings
-    WEBAPP_HOST = '0.0.0.0'
-    WEBAPP_PORT = int(os.getenv('PORT', 5000))
-
-    # start
-    try:
-        start_webhook(
-            dispatcher=dp,
-            webhook_path=WEBHOOK_PATH,
-            on_shutdown=on_shutdown,
-            on_startup=on_startup(bot, WEBHOOK_URL),
-            skip_updates=True,
-            host=WEBAPP_HOST,
-            port=WEBAPP_PORT
-        )
-    finally:
-        await dp.storage.close()
-        await dp.storage.wait_closed()
-
-
-if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        logger.error("Bot stopped!")
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_shutdown=on_shutdown,
+        on_startup=on_startup,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT
+    )
+    # except (KeyboardInterrupt, SystemExit):
+    #     logger.error("Bot stopped!")
